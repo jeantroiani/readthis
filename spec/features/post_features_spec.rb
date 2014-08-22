7d2e7377489b8dd54e140c9220aa6dda8aacf632
jeantroiani <digitalguest@gmail.com>
2014-08-22 13:54:45 +0100

    you can see posts in the root

--- spec/features/post_features_spec.rb
+++ spec/features/post_features_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 feature "PostFeatures", :type => :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+end
+
+describe 'Posts' do
+	
+	context 'With posts' do
+	
+		before(:each) do
+		Post.create( text: 'Hello World')
+		end
+	
+		it 'can be seen when you visit the index' do
+			visit('/posts')
+			expect(current_path) == posts_path
+			expect(page).to have_content('Hello World')
+		end 
+	end
+
+	context 'No post' do
+
+		it 'displays informative message' do
+			visit('/posts')
+			expect(current_path) == posts_path
+			expect(page).not_to have_content('Hello World')
+		end	
+	end
+
+	context 'Creating post' do
+
+		it 'users can create posts' do
+
+		end
+	
+	end 
+
+
 end
