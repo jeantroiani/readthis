8bbeb74097f9155105b00013d69fd092f4113550
jeantroiani <digitalguest@gmail.com>
2014-09-14 18:16:52 +0100

    review all the tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe Category, :type => :model do
 
 		describe 'categories' do
+		
 		before(:each) do
-			@category_1 = Category.create(tags: 'hello')
-			@category_2 = Category.create(tags: 'goodbye')
-			post_1 		  = Post.create(title: 'Hello World', category_id: @category_1.id )
-			post_2 		  = Post.create(title: 'Hello Moon' , category_id: @category_2.id )
-			post_3 		  = Post.create(title: 'Hello Marth', category_id: @category_2.id )			
+			@category_1 = Category.create(tags: 'Fashion')
+			@category_2 = Category.create(tags: 'Sports')
+			post_1 		  = Post.create(title: 'Hello World', 	category_id: @category_1.id )
+			post_2 		  = Post.create(title: 'Hello Moon' , 	category_id: @category_2.id )
+			post_3 		  = Post.create(title: 'Hello Marth', 	category_id: @category_2.id )			
 		end
 	
 		it 'can be ordered number of posts contained' do
